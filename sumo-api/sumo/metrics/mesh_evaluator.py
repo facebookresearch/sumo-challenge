Fixed rms color and points computation
Overlap threshold should only be used for similarity computation.
After a match is found, all points should be considered to compute the rms error.

sumo-api/sumo/metrics/mesh_evaluator.py
@@ -80,8 +80,8 @@ def sample_mesh(faces, density=625):
     faces (np array - 3*N x D) -  matrix representing vertices and faces with
     [X, Y, Z, ...].  faces[0:3, :] is the first face. N is the
     number of faces.  D >=3 (columns beyond 3 are interpolated, too)
-    density (float) - Number of points per square meter to sample.
-    Default 625 gives one point every 4 cm on average.
+    density (float) - Number of points per unit square surface to sample.
+    Default 625 gives one point every 4 cm on average in metric units.
 
     Return:
     samples (np array - N X D matrix of sampled points
@@ -100,9 +100,6 @@ def sample_mesh(faces, density=625):
     # normalized face area.
     Nsamples_per_face = np.clip(Nsamples_per_face, a_min=1, a_max=None)
     N = np.sum(Nsamples_per_face)  # N = total # of samples
-
-    if N == 0:
-        return np.empty((0, D), dtype=np.int64)
 
     face_ids = np.zeros((N,), dtype=np.int64)  # reserve space for result
 
@@ -185,8 +182,7 @@ class MeshEvaluator(Evaluator):
         Reference:
         https://www.cs.ox.ac.uk/files/7732/CS-RR-15-08.pdf
         """
-        return utils.points_rmsssd(self, self._submission, self._ground_truth,
-                                   self._settings["mesh_overlap_thresh"])
+        return utils.points_rmsssd(self, self._submission, self._ground_truth)
 
     def rms_color_error(self):
         """
@@ -199,8 +195,7 @@ class MeshEvaluator(Evaluator):
         Reference:
         https://www.cs.ox.ac.uk/files/7732/CS-RR-15-08.pdf
         """
-        return utils.color_rmsssd(self, self._submission, self._ground_truth,
-                                  self._settings["voxel_overlap_thresh"])
+        return utils.color_rmsssd(self, self._submission, self._ground_truth)
 
 #------------------------
 # End of public interface

sumo-api/sumo/metrics/utils.py
@@ -337,7 +337,7 @@ def nearest_neighbors(points1, points2):
     return ind1to2, ind2to1, dist1to2, dist2to1
 
 
-def points_rmsssd(evaluator, submission, ground_truth, overlap_thresh, voxels=False):
+def points_rmsssd(evaluator, submission, ground_truth, voxels=False):
     """
     Compute average root mean squared symmetric surface distance
     (RMSSSD). Equation 11 in SUMO white paper.
@@ -345,8 +345,6 @@ def points_rmsssd(evaluator, submission, ground_truth, overlap_thresh, voxels=Fa
     Inputs:
     submission (ProjectScene) - Submitted scene to be evaluated
     ground_truth (ProjectScene) - The ground truth scene
-    overlap_thresh (float) - max distance (meters) for establishing a
-      correspondence between sampled points
 
     Return:
         RMSSSD (float or math.inf if there are no corresponding points
@@ -389,11 +387,8 @@ def points_rmsssd(evaluator, submission, ground_truth, overlap_thresh, voxels=Fa
                 n_matched = dist1to2.shape[0] + dist2to1.shape[0]
                 if n_matched > 0: 
                     # SUMO white paper Eq 12
-                    rmsssd = np.sqrt(
-                        (np.sum(np.square(dist1to2[np.where(dist1to2.flatten() <=
-                           overlap_thresh)])) +
-                        np.sum(np.square(dist2to1[np.where(dist2to1.flatten() <=
-                           overlap_thresh)]))) / n_matched)
+                    rmsssd = np.sqrt((np.sum(np.square(dist1to2)) +
+                                      np.sum(np.square(dist2to1))) / n_matched)
                 else:
                     rmsssd = 0
                 rmsssd_cache[det_id][gt_id] = rmsssd
@@ -405,7 +400,7 @@ def points_rmsssd(evaluator, submission, ground_truth, overlap_thresh, voxels=Fa
         return math.inf  # no corrs found
 
 
-def color_rmsssd(evaluator, submission, ground_truth, overlap_thresh, voxels=False):
+def color_rmsssd(evaluator, submission, ground_truth, voxels=False):
     """
     Compute average root mean squared symmetric surface color distance
     (RMSSSCD). Equation 13 in SUMO white paper.
@@ -413,8 +408,6 @@ def color_rmsssd(evaluator, submission, ground_truth, overlap_thresh, voxels=Fal
     Inputs:
     submission (ProjectScene) - Submitted scene to be evaluated
     ground_truth (ProjectScene) - The ground truth scene
-    overlap_thresh (float) - max distance (meters) for establishing a
-      correspondence between sampled points
 
     Return:
         RMSSSCD (float or math.inf if there are no corresponding points
@@ -453,15 +446,9 @@ def color_rmsssd(evaluator, submission, ground_truth, overlap_thresh, voxels=Fal
 
                 idx1to2, idx2to1, dist1to2, dist2to1 = nearest_neighbors(
                     sub_points[:, 0:3], gt_points[:, 0:3])
-                idx_sub = np.argwhere((dist1to2 <=
-                    overlap_thresh).flatten()).flatten()  # vector
-                idx_gt = np.argwhere((dist2to1 <=
-                    overlap_thresh).flatten()).flatten()   # vector
 
-                color_diff1to2 = sub_points[idx_sub, 3:6] - \
-                    gt_points[idx1to2[idx_sub], 3:6]
-                color_diff2to1 = gt_points[idx_gt, 3:6] - \
-                    sub_points[idx2to1[idx_gt], 3:6]
+                color_diff1to2 = sub_points[:, 3:6] - gt_points[idx1to2, 3:6]
+                color_diff2to1 = gt_points[:, 3:6] - sub_points[idx2to1, 3:6]
 
                 n_matched = color_diff1to2.shape[0] + color_diff2to1.shape[0]
 